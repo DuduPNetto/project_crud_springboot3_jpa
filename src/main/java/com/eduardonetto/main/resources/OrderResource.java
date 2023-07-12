66760e09bf1f3a11f4e66586807c95aa02cba256
Eduardo Netto <dudupnetto.en@gmail.com>
2023-07-12 18:58:09 -0300

    Add CRUD Order

--- src/main/java/com/eduardonetto/main/resources/OrderResource.java
+++ src/main/java/com/eduardonetto/main/resources/OrderResource.java
@@ -1,13 +1,19 @@
 package com.eduardonetto.main.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.eduardonetto.main.entities.Order;
 import com.eduardonetto.main.services.OrderService;
@@ -28,6 +34,25 @@ public class OrderResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Order> findById(@PathVariable Long id) {
 		Order order = service.findById(id);
+		return ResponseEntity.ok().body(order);
+	}
+
+	@PostMapping
+	public ResponseEntity<Order> insert(@RequestBody Order order) {
+		order = service.insert(order);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(order.getId()).toUri();
+		return ResponseEntity.created(uri).body(order);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Order> update(@PathVariable Long id, @RequestBody Order order) {
+		order = service.update(id, order);
 		return ResponseEntity.ok().body(order);
 	}
 
--- src/main/java/com/eduardonetto/main/services/OrderService.java
+++ src/main/java/com/eduardonetto/main/services/OrderService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.eduardonetto.main.entities.Order;
 import com.eduardonetto.main.repositories.OrderRepository;
+import com.eduardonetto.main.services.exceptions.DatabaseException;
 import com.eduardonetto.main.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class OrderService {
@@ -23,6 +28,37 @@ public class OrderService {
 	public Order findById(Long id) {
 		Optional<Order> order = repository.findById(id);
 		return order.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public Order insert(Order order) {
+		return repository.save(order);
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public Order update(Long id, Order order) {
+		try {
+			Order entity = repository.getReferenceById(id);
+			updateData(entity, order);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Order entity, Order order) {
+		entity.setMoment(order.getMoment());
+		entity.setOrderStatus(order.getOrderStatus());
+		entity.setClient(order.getClient());
+		entity.setPayment(order.getPayment());
 	}
 
 }
